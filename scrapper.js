0e857a552a1878cf241454392444ade0776d4992
edgar <spickerman.edgar@gmail.com>
2017-10-09 00:41:47 -0700

    changed if to be a promise.all broke code down further

--- scrapper.js
+++ scrapper.js
@@ -2,16 +2,17 @@ const http = require('http');
 const fs = require('fs');
 const cheerio = require('cheerio');
 const json2csv = require('json2csv');
-fs.stat("./data", err => err ? fs.mkdir("./data") : '');
+let path;
 
 function scrapeUrl(url) {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
         const request = http.get(url, res => {
             let body = '';
             res.on('data', data => body += data.toString());
             res.on('end', () => resolve(body));
         });
         request.on('error', () => console.log("There's been a 404 error. Cannot connect to the to http://shirts4mike.com."));
+        request.once('response', res => path=request.path);
     });
 }
 
@@ -24,40 +25,45 @@ function getLinks(body) {
     return links;
 }
 
+function getShirtInfo(body) {
+    let $ = cheerio.load(body);
+    let shirt = {};
+    shirt.title = $('.shirt-details h1').text().slice(4);
+    shirt.price = $('.price').text();
+    shirt.imgUrl = 'http://www.shirts4mike.com/' + $('.shirt-picture span img').attr('src');
+    shirt.url = 'http://www.shirts4mike.com' + path;
+    shirt.date = new Date().toLocaleString();
+    return shirt;
+}
+
 function getInfo(links) {
     let shirts = [];
     for (let i = 0; i < links.length; i++) {
-        scrapeUrl(links[i]).then(body => {
-            let $ = cheerio.load(body);
-            let shirt = {};
-            shirt.title = $('.shirt-details h1').text().slice(4);
-            shirt.price = $('.price').text();
-            shirt.imgUrl = 'http://www.shirts4mike.com/' + $('.shirt-picture span img').attr('src');
-            shirt.url = links[i];
-            shirt.date = new Date().toLocaleString();
-            shirts.push(shirt);
-            return shirts;
-        }).then(print);
+        let shirtPromise = new Promise(resolve => resolve(scrapeUrl(links[i]).then(getShirtInfo)));
+        shirts.push(shirtPromise);
     }
+    Promise.all(shirts).then(print).catch(e => error(e));
 }
 
 function print(shirts) {
     let fields = ['title', 'price', 'imgUrl', 'url', 'date'];
     let csv = json2csv({ data: shirts, fields: fields });
-    if (shirts.length >= 8) {
-        let fileName = new Date().toLocaleDateString().replace(/\//g, '-');
-        fs.writeFile('./data/' + fileName + '.csv', csv, (err) => {
-            if (err) throw err;
-            console.log('file saved');
-        });
-    }
+    let fileName = new Date().toLocaleDateString().replace(/\//g, '-');
+    fs.writeFile('./data/' + fileName + '.csv', csv, err => {
+        if (err) throw err;
+        console.log('file saved');
+    });
 }
+
 function error(e) {
-    fs.appendFile('./scraper-error.log', new Date()+' '+e, (err) => {
+    console.log('There was an error please check scraper-error for more information');
+    fs.appendFile('./scraper-error.log', new Date() + ' ' + e + '\n', (err) => {
         if (err) throw err;
     });
 }
+
+fs.stat("./data", err => err ? fs.mkdir("./data") : '');
 scrapeUrl('http://www.shirts4mike.com/shirts.php')
     .then(getLinks)
     .then(getInfo)
-    .catch((e) => error(e));
+    .catch(e => error(e));
